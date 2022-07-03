06b41ab193c0ca9e503bb984c58ca4b962ac387c
meokisama <hoangdinhsang123@gmail.com>
2022-07-03 14:11:44 +0700

    Modify 404 for Github Pages

--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -6,6 +6,14 @@ import Testimonial from "../elements/Testimonial";
 const sliderData = [
   {
     id: 1,
+    avatar: "images/avatar-3.png",
+    userName: "Tsurumaru Kuninaga",
+    subtitle: "Touken Ranbu",
+    review:
+      "Đã là cuộc sống thì phải có bất ngờ chứ. Nếu mọi chuyện xảy ra đều có thể dự đoán, trái tim tôi sẽ khô héo mà chết trước mất thôi.",
+  },
+  {
+    id: 2,
     avatar: "images/avatar-1.png",
     userName: "Atsuki Okano",
     subtitle: "Hajime-chan Ga Ichiban!",
@@ -13,21 +21,14 @@ const sliderData = [
       'Không phải là "không được", mà chỉ là "chưa thử" thôi, đúng không ?',
   },
   {
-    id: 2,
+    id: 3,
     avatar: "images/avatar-2.png",
     userName: "Machi Tokiwa",
     subtitle: "Hanayamata",
     review:
       "Cậu là người duy nhất biết được niềm vui của mình là gì mà, nhưng tớ nghĩ có một người chấp nhận lựa chọn của cậu, cười và khóc cùng cậu, thì đúng là một điều tuyệt vời.",
   },
-  {
-    id: 3,
-    avatar: "images/avatar-3.png",
-    userName: "Tsurumaru Kuninaga",
-    subtitle: "Touken Ranbu",
-    review:
-      "Đã là cuộc sống thì phải có bất ngờ chứ. Nếu mọi chuyện xảy ra đều có thể dự đoán, trái tim tôi sẽ khô héo mà chết trước mất thôi.",
-  },
+  
 ];
 
 function Testimonials() {
